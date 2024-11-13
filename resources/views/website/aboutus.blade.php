f3744b985057a715ff3bc55c29abad31d92fbab7
Hillary Ngige <hilaryme45@gmail.com>
2024-11-13 20:26:43 +0300

    Added the about us page

---
+++ resources/views/website/aboutus.blade.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+{{-- header title favicon etc --}}
+<x-header title="About Chantilly Schools" />
+
+<body>
+    {{-- mobile menu and desktop menu --}}
+    <x-menu />
+
+    {{-- BODY STARTS HERE --}}
+    <!--Breadcrumb Banner Area Start-->
+    <div class="breadcrumb-banner-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="breadcrumb-text">
+                        <h1 class="text-center">About Us</h1>
+                        <div class="breadcrumb-bar">
+                            <ul class="breadcrumb">
+                                <li><a href="/">Home</a></li>
+                                <li>About Us</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--End of Breadcrumb Banner Area-->
+
+    <!--About Area Start-->
+    <div class="about-area section-padding-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title-wrapper">
+                        <div class="section-title">
+                            <h3>OUR HISTORY</h3>
+                            <p>Find out where, when and how we started and became.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="about-text-container">
+                        <p>A young, passionate teacher named Mrs. Nyambura had a vision. She dreamed of creating a
+                        school that would be more than just an educational institution; she wanted it to be a
+                        nurturing environment where children could explore, learn, and grow into well-rounded
+                        individuals.</p>
+
+                        <p>With limited resources and a strong spirit, Mrs. Nyambura rented a small, single-room
+                        building and named it Chantilly School. On the first day, only two students, siblings named
+                        Amani and Lila, showed up, clutching their worn notebooks and looking curiously at the
+                        modest space that would become their second home. Mrs. Nyambura greeted them warmly, her
+                        heart full of hope despite the humble start.</p>
+
+                        <p>The early days were challenging. Mrs. Nyambura not only taught all the subjects but also
+                        cleaned the classroom and prepared lessons late into the night. Word began to spread about
+                        her innovative teaching methods, which involved storytelling, songs, and hands-on
+                        activities. Parents who saw the joy in Amani and Lila’s faces after school soon became
+                        curious, bringing their children to witness this new approach.</p>
+
+                        <p>Within a year, the two students turned into twenty. The single room could no longer hold the
+                        growing class, so Mrs. Nyambura, with the help of supportive parents and community members,
+                        expanded the school to include two more rooms. Chantilly School's reputation for excellence
+                        began to spread throughout Maplewood and beyond.</p>
+
+                        <p>Five years later, Chantilly School had 200 students enrolled. The school boasted dedicated
+                        teachers who shared Mrs. Nyambura’s vision of providing not just education but life skills
+                        and values. They introduced extracurricular programs like music, art, and sports, nurturing
+                        talents that thrived alongside academic achievement.</p>
+
+                        <p>As the years passed, Chantilly School became a pillar of the community. By its tenth
+                        anniversary, the school reached a remarkable milestone: 1,000 students enrolled across
+                        different grades. The once-tiny single-room school was now a campus with multiple buildings,
+                        playgrounds, and state-of-the-art facilities.</p>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="skill-image my-auto">
+                        <img src="/img/class/chantillyadmin.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--End of About Area-->
+    
+    <!--Teachers Column Carousel Start-->
+    <div class="teachers-column-carousel-area section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title-wrapper">
+                        <div class="section-title">
+                            <h3>Our Awards</h3>
+                            <p>We are glad to showcase our awards a true testament of our prowess.</p>
+                        </div>
+                    </div> 
+                </div>       
+            </div>
+            <div class="teachers-column-carousel carousel-style-one owl-carousel">
+                <div class="single-teachers-column text-center">
+                    <div class="teachers-image-column">
+                        <a href="#">
+                            <img src="img/class/trophy.jpg" alt="">
+                            <span class="image-hover">
+                                <span>25th Jan 2024</span>
+                            </span>
+                        </a>
+                    </div>
+                    <div class="teacher-column-carousel-text">
+                        <h4>Music Award</h4>
+                        <span>20th June 2024</span>
+                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                    </div>
+                </div>
+                <div class="single-teachers-column text-center">
+                    <div class="teachers-image-column">
+                        <a href="#">
+                            <img src="img/class/trophy.jpg" alt="">
+                            <span class="image-hover">
+                                <span>25th Jan 2024</span>
+                            </span>
+                        </a>
+                    </div>
+                    <div class="teacher-column-carousel-text">
+                        <h4>Drama Award</h4>
+                        <span>20th June 2024</span>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry, has been the industry's standard dummy.</p>
+                    </div>
+                </div>
+                <div class="single-teachers-column text-center">
+                    <div class="teachers-image-column">
+                        <a href="#">
+                            <img src="img/class/trophy.jpg" alt="">
+                            <span class="image-hover">
+                                <span>25th Jan 2024</span>
+                            </span>
+                        </a>
+                    </div>
+                    <div class="teacher-column-carousel-text">
+                        <h4>Best School In Kenya</h4>
+                        <span>20th June 2024</span>
+                        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature</p>
+                    </div>
+                </div>
+                <div class="single-teachers-column text-center">
+                    <div class="teachers-image-column">
+                        <a href="#">
+                            <img src="img/class/trophy.jpg" alt="">
+                            <span class="image-hover">
+                                <span>25th Jan 2025</span>
+                            </span>
+                        </a>
+                    </div>
+                    <div class="teacher-column-carousel-text">
+                        <h4>Best in Football</h4>
+                        <span>20th June 2024</span>
+                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some.</p>
+                    </div>
+                </div>
+                <div class="single-teachers-column text-center">
+                    <div class="teachers-image-column">
+                        <a href="#">
+                            <img src="img/class/trophy.jpg" alt="">
+                            <span class="image-hover">
+                                <span>25th Jan 2024</span>
+                            </span>
+                        </a>
+                    </div>
+                    <div class="teacher-column-carousel-text">
+                        <h4>Best In Kiswahili</h4>
+                        <span>20th June 2024</span>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry, has been the industry's standard dummy.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--End of Teachers Column Carousel-->
+    {{-- BODY ENDS HERE --}}
+
+    {{-- FOOTER --}}
+    <x-footer page="aboutus" />
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('website.homepage');
 });
+Route::get("/AboutUs", function () {
+    return view('website.aboutus');
+});
