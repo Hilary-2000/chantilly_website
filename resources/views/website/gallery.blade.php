fffb848f9d7d7763ca423375d426cff420708a30
Hillary Ngige <hilaryme45@gmail.com>
2024-11-16 11:15:01 +0300

    Completed galleries

--- resources/views/website/gallery.blade.php
+++ resources/views/website/gallery.blade.php
@@ -8,26 +8,138 @@
     <x-menu />
 
     {{-- BODY STARTS HERE --}}
-
-    <!--Breadcrumb Banner Area Start-->
-    <div class="breadcrumb-banner-area">
+    <!--Gallery Area Start-->
+    <div class="gallery-area section-padding gallery-full-width">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <div class="breadcrumb-text">
-                        <h1 class="text-center">Gallery</h1>
-                        <div class="breadcrumb-bar">
-                            <ul class="breadcrumb">
-                                <li><a href=".">Home</a></li>
-                                <li>Gallery</li>
-                            </ul>
+                    <div class="filter-menu">
+                        <ul>
+                            <li class="filter" data-filter="all">All</li>
+                            <li class="filter" data-filter=".compound">School Compound</li>
+                            <li class="filter" data-filter=".graduation">Graduation</li>
+                            <li class="filter" data-filter=".classes">Classes</li>
+                            <li class="filter" data-filter=".play">Play time</li>
+                            <li class="filter" data-filter=".amenities">Amenities</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="filter-items">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-12 mix single-items compound overlay-hover">
+                        <div class="overlay-effect">
+                            <a aria-disabled="true" tabindex="-1"><img src="img/gallery/1.jpg" alt=""></a>
+                            <div class="gallery-hover-effect">
+                                <a class="gallery-icon venobox" href="img/gallery/1.jpg"><i class="fa fa-image"></i></a>
+                                <span class="gallery-text">Drawings</span>
+                            </div> 
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-12 mix single-items play amenities overlay-hover">
+                        <div class="overlay-effect">
+                            <a aria-disabled="true" tabindex="-1"><img src="img/gallery/2.jpg" alt=""></a>
+                            <div class="gallery-hover-effect">
+                                <a class="gallery-icon venobox" href="img/gallery/2.jpg"><i class="fa fa-image"></i></a>
+                                <span class="gallery-text">Activities, Photos</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-12 mix single-items play overlay-hover">
+                        <div class="overlay-effect">
+                            <a aria-disabled="true" tabindex="-1"><img src="img/gallery/3.jpg" alt=""></a>
+                            <div class="gallery-hover-effect">
+                                <a class="gallery-icon venobox" href="img/gallery/3.jpg"><i class="fa fa-image"></i></a>
+                                <span class="gallery-text">Play Time</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-12 mix single-items play graduation overlay-hover">
+                        <div class="overlay-effect">
+                            <a aria-disabled="true" tabindex="-1"><img src="img/gallery/4.jpg" alt=""></a>
+                            <div class="gallery-hover-effect">
+                                <a class="gallery-icon venobox" href="img/gallery/4.jpg"><i class="fa fa-image"></i></a>
+                                <span class="gallery-text">Excursions, Play</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-12 mix single-items classes amenities overlay-hover">
+                        <div class="overlay-effect">
+                            <a aria-disabled="true" tabindex="-1"><img src="img/gallery/5.jpg" alt=""></a>
+                            <div class="gallery-hover-effect">
+                                <a class="gallery-icon venobox" href="img/gallery/5.jpg"><i class="fa fa-image"></i></a>
+                                <span class="gallery-text">Courses, Exursions</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-12 mix single-items compound overlay-hover">
+                        <div class="overlay-effect">
+                            <a aria-disabled="true" tabindex="-1"><img src="img/gallery/12.jpg" alt=""></a>
+                            <div class="gallery-hover-effect">
+                                <a class="gallery-icon venobox" href="img/gallery/12.jpg"><i class="fa fa-image"></i></a>
+                                <span class="gallery-text">Drawing</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-12 mix single-items classes amenities overlay-hover">
+                        <div class="overlay-effect">
+                            <a aria-disabled="true" tabindex="-1"><img src="img/gallery/6.jpg" alt=""></a>
+                            <div class="gallery-hover-effect">
+                                <a class="gallery-icon venobox" href="img/gallery/6.jpg"><i class="fa fa-image"></i></a>
+                                <span class="gallery-text">Excursions, Courses</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-12 mix single-items graduation overlay-hover">
+                        <div class="overlay-effect">
+                            <a aria-disabled="true" tabindex="-1"><img src="img/gallery/10.jpg" alt=""></a>
+                            <div class="gallery-hover-effect">
+                                <a class="gallery-icon venobox" href="img/gallery/10.jpg"><i class="fa fa-image"></i></a>
+                                <span class="gallery-text">Drawing, Exursions</span>
+                            </div>
+                        </div>
+                    </div>  
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-12 mix single-items compound overlay-hover">
+                        <div class="overlay-effect">
+                            <a aria-disabled="true" tabindex="-1"><img src="img/gallery/7.jpg" alt=""></a> 
+                            <div class="gallery-hover-effect">
+                                <a class="gallery-icon venobox" href="img/gallery/7.jpg"><i class="fa fa-image"></i></a>
+                                <span class="gallery-text">Drawings</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-12 mix single-items amenities classes overlay-hover">
+                        <div class="overlay-effect">
+                            <a aria-disabled="true" tabindex="-1"><img src="img/gallery/10.jpg" alt=""></a>
+                            <div class="gallery-hover-effect">
+                                <a class="gallery-icon venobox" href="img/gallery/10.jpg"><i class="fa fa-image"></i></a>
+                                <span class="gallery-text">Activities, Photos</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-12 mix single-items amenities overlay-hover">
+                        <div class="overlay-effect">
+                            <a aria-disabled="true" tabindex="-1"><img src="img/gallery/11.jpg" alt=""></a>
+                            <div class="gallery-hover-effect">
+                                <a class="gallery-icon venobox" href="img/gallery/11.jpg"><i class="fa fa-image"></i></a>
+                                <span class="gallery-text">Play Time</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-12 mix single-items play graduation overlay-hover">
+                        <div class="overlay-effect">
+                            <a aria-disabled="true" tabindex="-1"><img src="img/gallery/8.jpg" alt=""></a>
+                            <div class="gallery-hover-effect">
+                                <a class="gallery-icon venobox" href="img/gallery/8.jpg"><i class="fa fa-image"></i></a>
+                                <span class="gallery-text">Excursions, Play</span>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <!--End of Breadcrumb Banner Area-->
+    <!--End of Gallery Area-->
     {{-- BODY ENDS HERE --}}
 
     {{-- FOOTER --}}
